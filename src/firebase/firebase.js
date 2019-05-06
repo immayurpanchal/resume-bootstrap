4822dd824179d5ae5b8490db3694107942c89aea
_immayurpanchal <mayur.panchal@vuclip.com>
2019-05-06 18:48:52 +0530

    sometimes it is neccessary to commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
